54033ccd1df1b5aaee2f34cd6d45c73f48daebeb
Shawn Hartsock <hartsock@acm.org>
2012-03-19 00:41:07 -0400

    initial functional implementation

--- src/main/java/com/blogspot/hartsock/functionalthinking/LinkedMap.java
+++ src/main/java/com/blogspot/hartsock/functionalthinking/LinkedMap.java
@@ -10,7 +10,7 @@ public class LinkedMap implements ObjectMap {
 	
 	class Link extends Pair {
 		public Link(Object left, Object right) {super(left, right);}
-		Link next;
+		Link next = null;
 	}
 	
 	Link head = new Link(null,null);
@@ -20,37 +20,66 @@ public class LinkedMap implements ObjectMap {
 		if(key == null) {
 			throw new NullKeyIsIllegal();
 		}
-		Link last = null;
-		Link current = head;
-		while (current != null && !current.keyIs(key)) {
-			last = current;
-			current = current.next;
-		}
+		Link current = filter(key, new Actor() {
+			public Link action(Object key, Link link) {
+				return link.keyIs(key) ? link : null;
+			}
+		});
 		if (current != null) {
 			Link replacement = new Link(current.getKey(), value);
 			replacement.next = current.next;
+			Link last = filter(current, new Actor() {
+				public Link action(Object current, Link link) {
+					return link.next == current ? link : null;
+				}
+			});
 			if (last != null) {
 				last.next = replacement;
 			} else {
 				head = replacement;
 			}
 		} else {
-			last.next = new Link(key, value);
+			Link tail = new Link(key, value);
+			filter(tail,new Actor() {
+				public Link action(Object tail, Link current) {
+					if(current.next == null) {
+						current.next = (Link) tail;
+						return (Link) tail;
+					}
+					else 
+						return null;
+				}
+			});
 		}
 	}
 
+	interface Actor {
+		/** non-null return means stop */
+		Link action(Object criteria, Link link);
+	}
+	
+	Link filter(Object criteria, Actor actor) {
+		Link result = null;
+		Link it = this.head;
+		while(it != null) {
+			result = actor.action(criteria, it);
+			if(result != null) {
+				break;
+			}
+			it = it.next;
+		}
+		return result;
+	}
+	
 	@Override
 	public Object get(Object key) {
-		Link current = head;
+		Link current = filter(key, new Actor() {
+			public Link action(Object key, Link link) {
+				return link.keyIs(key) ? link : null;
+			}
+		});
 		
-		while(current != null && !current.keyIs(key)) {
-			current = current.next;
-		}
-		
-		if(current != null)
-			return current.getValue();
-		else
-			return null;
+		return (current != null) ? current.getValue() : null;
 	}
 
 }
