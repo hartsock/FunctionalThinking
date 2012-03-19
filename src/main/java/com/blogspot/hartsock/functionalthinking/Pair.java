11b5037191078b57d54d5dded55f3b17519a77ff
Shawn Hartsock <hartsock@acm.org>
2012-03-18 22:29:33 -0400

    my understanding of a monadic type design created simply within the Pair class

--- src/main/java/com/blogspot/hartsock/functionalthinking/Pair.java
+++ src/main/java/com/blogspot/hartsock/functionalthinking/Pair.java
@@ -1,12 +1,20 @@
 package com.blogspot.hartsock.functionalthinking;
 
 /**
- * @author mansoor
+ * @author mansoor, hartsock
  * @since 2/7/12
+ * <p/>
+ * In an attempt to illustrate monadic type design in code very simply
+ * I've extended the Pair type with a NonPair type.
  */
 public class Pair<L,R> {
-   private  L left;
-    private  R right;
+	@SuppressWarnings("serial")
+	public static class HasNoLeft extends RuntimeException {};
+	@SuppressWarnings("serial")
+	public static class HasNoRight extends RuntimeException {};
+	
+	private final L left;
+    private final R right;
 
     public Pair(L left, R right) {
         this.left = left;
@@ -20,12 +28,26 @@ public class Pair<L,R> {
     public R getRight() {
         return right;
     }
-
-    public void setLeft(L left) {
-        this.left = left;
-    }
-
-    public void setRight(R right) {
-        this.right = right;
+    
+    public boolean hasLeft() { return left != null; }
+    public boolean hasRight() { return right != null; }
+ 
+    /**
+     * A non-valid container value NonPair is in the categorical
+     * unit of Pair but still outside the range of valid Pair
+     * combinations.
+     * 
+     * @author hartsock
+     */
+    public class NonPair extends Pair<L,R> {
+    		public NonPair(L left, R right) {
+    			super(left, right);
+    		}
+    		
+    		public L getLeft() { throw new HasNoLeft(); }
+    		public R getRight() { throw new HasNoRight(); }
+    	
+    		public boolean hasLeft() { return false; }
+    		public boolean hasRight() { return false; }
     }
 }
