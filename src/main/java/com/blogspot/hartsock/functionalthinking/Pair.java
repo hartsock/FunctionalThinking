6682693e8c8563462b511afd4ef88493371da46d
Shawn Hartsock <hartsock@acm.org>
2012-03-18 22:36:44 -0400

    formatting

--- src/main/java/com/blogspot/hartsock/functionalthinking/Pair.java
+++ src/main/java/com/blogspot/hartsock/functionalthinking/Pair.java
@@ -7,47 +7,38 @@ package com.blogspot.hartsock.functionalthinking;
  * In an attempt to illustrate monadic type design in code very simply
  * I've extended the Pair type with a NonPair type.
  */
-public class Pair<L,R> {
-	@SuppressWarnings("serial")
+public class Pair<L, R> {
 	public static class HasNoLeft extends RuntimeException {};
-	@SuppressWarnings("serial")
 	public static class HasNoRight extends RuntimeException {};
-	
+
 	private final L left;
-    private final R right;
+	private final R right;
 
-    public Pair(L left, R right) {
-        this.left = left;
-        this.right = right;
-    }
+	public Pair(L left, R right) {
+		this.left = left;
+		this.right = right;
+	}
 
-    public L getLeft() {
-        return left;
-    }
+	public L getLeft() {	return left;}
+	public R getRight() {return right;}
+	public boolean hasLeft() {return left != null;}
+	public boolean hasRight() {return right != null;}
 
-    public R getRight() {
-        return right;
-    }
-    
-    public boolean hasLeft() { return left != null; }
-    public boolean hasRight() { return right != null; }
- 
-    /**
-     * A non-valid container value NonPair is in the categorical
-     * unit of Pair but still outside the range of valid Pair
-     * combinations.
-     * 
-     * @author hartsock
-     */
-    public class NonPair extends Pair<L,R> {
-    		public NonPair(L left, R right) {
-    			super(left, right);
-    		}
-    		
-    		public L getLeft() { throw new HasNoLeft(); }
-    		public R getRight() { throw new HasNoRight(); }
-    	
-    		public boolean hasLeft() { return false; }
-    		public boolean hasRight() { return false; }
-    }
+	/**
+	 * A non-valid container value NonPair is in the categorical unit of Pair
+	 * but still outside the range of valid Pair combinations.
+	 * 
+	 * @author hartsock
+	 */
+	public class NonPair extends Pair<L, R> {
+		public NonPair(L left, R right) {	super(left, right);	}
+
+		public L getLeft() {	throw new HasNoLeft();	}
+
+		public R getRight() {	throw new HasNoRight();	}
+
+		public boolean hasLeft() {	return false;	}
+
+		public boolean hasRight() {	return false;	}
+	}
 }
